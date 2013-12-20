6ff749b4e19bae6ae462593094bf6721bcfb1998
Troels Irgens-Møller <troelsirgensmoeller@gmail.com>
2013-12-20 11:25:56 +0100

    Passports in database, validating

--- app.rb
+++ app.rb
@@ -21,6 +21,7 @@ configure :production do
 		:database => db.path[1..-1],
 		:encoding => 'utf8'
 	)
+	ActiveRecord::Base.logger.level=1
 end
 
 configure do
@@ -28,6 +29,9 @@ configure do
 	set :server_dir, Dir.pwd
 	@server_dir =  Dir.pwd
 end
+
+# Models
+
 
 class Company < ActiveRecord::Base
 	has_many :owners
@@ -41,11 +45,7 @@ class Owner < ActiveRecord::Base
 	validates :name, presence: true
 	
 	def has_passport?
-		return File.file?("#{Dir.pwd}#{self.passport}")
-	end
-
-	def passport_path
-		return self.has_passport? ? "#{Dir.pwd}#{self.passport}" : nil
+		return self.passport != nil
 	end
 end
 
@@ -112,7 +112,7 @@ post "/owners" do
 	company_id = jparams['owner']['company_id']
 	@company = Company.find(company_id)
 	@owner=@company.owners.new(name: jparams['owner']['name'])
-	passport = save_passport(@owner,jparams)
+	passport = get_passport(jparams)
 	@owner.update_attributes(passport: passport)
 	@company.save!
 	status 201
@@ -122,14 +122,15 @@ end
 put "/owners/:id" do
 	jparams = JSON.parse(request.body.read.to_s)
 	@owner = Owner.find(params[:id])
-	passport = save_passport(@owner,jparams)
+	passport = get_passport(jparams)
 	@owner.update_attributes(name: jparams["owner"]["name"], passport: passport)
 	jbuilder :owner_show
 end
 
 get "/owners/:id/passport.pdf" do
 	@owner = Owner.find(params[:id])
-	send_file @owner.passport_path
+	content_type 'application/pdf'
+	response.write(Base64.urlsafe_decode64(@owner.passport))
 end
 
 delete "/owners/:id" do
@@ -151,28 +152,14 @@ helpers do
 		"#{get_base_url}/owners/#{owner.id}/passport.pdf"
 	end
 
-	def save_passport(owner, jparams) # Saves the file in jparams if there is one, returns 400 if it's invalid
-		if jparams["owner"]["passport_file"]
-			owner_id = params[:id]
-			folder = "/passports"
-			timestamp = Time.now.strftime("%Y-%m-%d-%H-%M-%S.%L")
-			filename = "#{folder}/owner-#{owner_id}-#{timestamp}.pdf"
-			print "Writing to #{filename}..."
-			begin
-				File.open("#{settings.server_dir}#{filename}", 'w') do |f|
-					# Strip the data type by finding where the "real data" starts
-					start = jparams["owner"]["passport_file"].index("base64,")+7
-					f.write(Base64.urlsafe_decode64(jparams["owner"]["passport_file"][start .. -1]))
-				end
-			rescue ArgumentError # If the data is not valid Base64
-				puts "error."
-				halt 400
-			end
-			puts "done."
-			# Return *relative* file path
-			return filename
+	def get_passport(jparams) # Saves the file in jparams if there is one, returns 400 if it's invalid
+		start = jparams["owner"]["passport_file"].index("base64,")+7
+		intro = jparams["owner"]["passport_file"][0 .. start]
+		if intro.match("application/pdf") or intro.match("application/x-pdf")
+			return jparams["owner"]["passport_file"][start .. -1]
 		else
-			return nil
+			raise ArgumentError, "Passport Not PDF"
 		end
 	end
+	
 end
